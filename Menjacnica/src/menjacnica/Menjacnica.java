8ae19410f894e353d83f0f1c64ef703134a9fdc0
dzonidjo <nikola.djo.okic@gmail.com>
2018-03-20 00:41:44 +0100

    Popunjene metode.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,5 +1,28 @@
 package menjacnica;
 
-public class Menjacnica {
+import java.util.GregorianCalendar;
+
+import interfejs.IMenjacnica;
+
+public class Menjacnica implements IMenjacnica{
+
+	@Override
+	public void dodajKurs(String naziv, String skraceniNaziv, GregorianCalendar datum, double prodajniKurs,
+			double kupovniKurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(String skraceniNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Valuta vratiKurs(String skraceniNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
